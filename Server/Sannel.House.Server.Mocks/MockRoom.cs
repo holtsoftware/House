dfdc3742d4d4b993921c3c82ea5e8feb1a1e05f9
adam.holt <adam.holt@mccann.com>
2013-12-11 17:10:06 -0700

    Added missing files.

---
+++ Server/Sannel.House.Server.Mocks/MockRoom.cs
@@ -0,0 +1,43 @@
+﻿using Sannel.House.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sannel.House.Server.Mocks
+{
+	public class MockRoom : IRoom
+	{
+		public Guid RoomId
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public string Description
+		{
+			get;
+			set;
+		}
+
+		public int Order
+		{
+			get;
+			set;
+		}
+
+		private List<ICircuit> circuits = new List<ICircuit>();
+
+		public ICollection<ICircuit> Circuits
+		{
+			get { return circuits; }
+		}
+	}
+}
