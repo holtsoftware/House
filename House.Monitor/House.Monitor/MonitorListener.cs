a3135a0a62d35f7646bfe0e8674aeb12398a78bc
Adam Holt <holtsoftware@outlook.com>
2015-07-28 21:16:49 -0600

    ?

---
+++ House.Monitor/House.Monitor/MonitorListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+
+namespace House.Monitor
+{
+	public partial class MonitorListener : ServiceBase
+	{
+		public MonitorListener()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			UdpClient client = new UdpClient();
+			client.Connect(IPAddress.Broadcast, )
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
