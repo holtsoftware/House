1619f9c28efa9165dd9019e7c64ad08e2b475972
Adam Holt <holtsoftware@outlook.com>
2016-07-01 22:34:50 -0600

    Added some projects including the test project.

---
+++ Sannel.House.Web/src/Sannel.House.Web.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Common;
+using NUnit.Framework;
+using System.Reflection;
+using NUnitLite;
+
+namespace Sannel.House.Web.Tests
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			int result = new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+				.Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+			Console.ReadKey();
+
+			return result;
+		}
+	}
+}
