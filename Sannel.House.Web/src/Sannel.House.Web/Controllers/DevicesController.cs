9931d6b9128da7715ddc5f0b0f7ac76b79c825ea
Adam Holt <holtsoftware@outlook.com>
2016-07-12 13:13:35 -0600

    Updated How Devices are handled

--- Sannel.House.Web/src/Sannel.House.Web/Controllers/DevicesController.cs
+++ Sannel.House.Web/src/Sannel.House.Web/Controllers/DevicesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Sannel.House.Web.Base.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
--- Sannel.House.Web/src/Sannel.House.Web/wwwroot/js/site.js
+++ Sannel.House.Web/src/Sannel.House.Web/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your Javascript code.
+﻿/// <reference path="jquery-3.0.0.js" />
+/// <reference path="knockout-3.4.0.js" />
+
+function Device(){
+    this.Id = 0;
+    this.Name = "";
+    this.Description = "";
+}
+
+function DevicesViewModel() {
+    this.ApiUrl = "/api/devices";
+    this.Devices = ko.observableArray([]);
+}
+
+DevicesViewModel.prototype.LoadDevices = function () {
+    $.ajax({
+        url: this.ApiUrl,
+        type: 'get',
+        success: function (data) {
+            console.log(data);
+        }
+    })
+}
