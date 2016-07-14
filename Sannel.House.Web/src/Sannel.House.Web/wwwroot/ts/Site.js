fd130b717c9cd167b5db22d6e4ca67ad2be2eaa7
Adam Holt <holtsoftware@outlook.com>
2016-07-13 23:17:11 -0600

    Got adding device working again on the client side.

--- Sannel.House.Web/src/Sannel.House.Web/wwwroot/ts/Site.js
+++ Sannel.House.Web/src/Sannel.House.Web/wwwroot/ts/Site.js
@@ -1,19 +1,47 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="knockout.d.ts" />
+var ServerDevice = (function () {
+    function ServerDevice() {
+    }
+    return ServerDevice;
+}());
+var Device = (function () {
+    function Device(sdevice) {
+        if (sdevice === void 0) { sdevice = undefined; }
+        this.Id = ko.observable(0);
+        this.Name = ko.observable("");
+        this.Description = ko.observable("");
+        this.DisplayOrder = ko.observable(0);
+        if (sdevice != undefined) {
+            this.Id(sdevice.Id);
+            this.Name(sdevice.Name);
+            this.Description(sdevice.Description);
+            this.DisplayOrder(sdevice.DisplayOrder);
+        }
+    }
+    Device.prototype.AsServerDevice = function () {
+        var sd = new ServerDevice();
+        sd.Id = this.Id();
+        sd.Name = this.Name();
+        sd.Description = this.Description();
+        sd.DisplayOrder = this.DisplayOrder();
+        return sd;
+    };
+    return Device;
+}());
 var DevicesViewModel = (function () {
     function DevicesViewModel() {
         this.Devices = ko.observableArray([]);
+        this.CurrentDevice = ko.observable(undefined);
         this._apiUrl = "/api/Devices";
     }
     DevicesViewModel.prototype._devicesReceived = function (data) {
-        console.log(data);
         for (var i = 0; i < data.length; i++) {
-            this.Devices.push(data[i]);
+            this.Devices.push(new Device(data[i]));
         }
     };
     DevicesViewModel.prototype.loadDevices = function () {
         var me = this;
-        console.log(me._apiUrl);
         $.ajax({
             url: me._apiUrl,
             type: 'get',
@@ -22,6 +50,57 @@ var DevicesViewModel = (function () {
                 me._devicesReceived(data);
             }
         });
+    };
+    DevicesViewModel.prototype.loadDevice = function (id) {
+        var me = this;
+        $.ajax({
+            url: me._apiUrl + "/" + id,
+            type: 'get',
+            contentType: "application/json; charset=utf-8",
+            success: function (device) {
+                var found = false;
+                for (var i = 0; i < me.Devices().length; i++) {
+                    var d = me.Devices()[i];
+                    if (d.Id() == device.Id) {
+                        me.Devices.replace(d, new Device(device));
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    me.Devices.push(new Device(device));
+                }
+            }
+        });
+    };
+    DevicesViewModel.prototype.AddNewDevice = function () {
+        this.CurrentDevice(new Device());
+    };
+    DevicesViewModel.prototype.SaveNewDevice = function () {
+        var cd = this.CurrentDevice();
+        var errorMessage = "";
+        if (cd.Name().length <= 0) {
+            errorMessage = "Name is required.\n";
+        }
+        if (cd.Description().length <= 0) {
+            errorMessage += "Description is required.\n";
+        }
+        if (errorMessage.length > 0) {
+            alert(errorMessage);
+        }
+        else {
+            var me = this;
+            $.ajax({
+                url: me._apiUrl,
+                type: 'post',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(cd.AsServerDevice()),
+                dataType: "json",
+                success: function (id) {
+                    me.loadDevice(id);
+                }
+            });
+        }
     };
     return DevicesViewModel;
 }());
--- Sannel.House.Web/src/Sannel.House.Web/wwwroot/ts/Site.ts
+++ Sannel.House.Web/src/Sannel.House.Web/wwwroot/ts/Site.ts
@@ -1,38 +1,117 @@
 ﻿/// <reference path="jquery.d.ts" />
 /// <reference path="knockout.d.ts" />
 
-interface Device {
-    Id: Number;
-    Name: String;
-    Description: String;
-    DisplayOrder: Number;
+class ServerDevice {
+	Id: Number;
+	Name: string;
+	Description: string;
+	DisplayOrder: Number;
+}
+
+class Device {
+	Id: KnockoutObservable<Number> = ko.observable(0);
+	Name: KnockoutObservable<string> = ko.observable("");
+	Description: KnockoutObservable<string> = ko.observable("");
+	DisplayOrder: KnockoutObservable<Number> = ko.observable(0);
+
+	constructor(sdevice: ServerDevice = undefined) {
+		if (sdevice != undefined) {
+			this.Id(sdevice.Id);
+			this.Name(sdevice.Name);
+			this.Description(sdevice.Description);
+			this.DisplayOrder(sdevice.DisplayOrder);
+		}
+	}
+
+	public AsServerDevice(): ServerDevice {
+		var sd = new ServerDevice();
+		sd.Id = this.Id();
+		sd.Name = this.Name();
+		sd.Description = this.Description();
+		sd.DisplayOrder = this.DisplayOrder();
+		return sd;
+	}
 }
 
 class DevicesViewModel {
-    private _apiUrl: string;
-    public Devices: KnockoutObservableArray<Device> = ko.observableArray([]);
+	private _apiUrl: string;
+	public Devices: KnockoutObservableArray<Device> = ko.observableArray([]);
+	public CurrentDevice: KnockoutObservable<Device> = ko.observable(undefined);
 
-    constructor() {
-        this._apiUrl = "/api/Devices";
-    }
+	constructor() {
+		this._apiUrl = "/api/Devices";
+	}
 
-    private _devicesReceived(data: Device[]) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            this.Devices.push(data[i]);
-        }
-    }
+	private _devicesReceived(data: ServerDevice[]) {
+		for (var i = 0; i < data.length; i++) {
+			this.Devices.push(new Device(data[i]));
+		}
+	}
 
-    loadDevices() {
-        var me = this;
-        console.log(me._apiUrl);
-        $.ajax({
-            url: me._apiUrl,
-            type: 'get',
-            contentType: "application/json; charset=utf-8",
-            success: function (data) {
-                me._devicesReceived(data);
-            }
-        });
-    }
+	public loadDevices() {
+		var me = this;
+		$.ajax({
+			url: me._apiUrl,
+			type: 'get',
+			contentType: "application/json; charset=utf-8",
+			success: function (data) {
+				me._devicesReceived(data);
+			}
+		});
+	}
+
+	public loadDevice(id: Number) {
+		var me = this;
+		$.ajax({
+			url: me._apiUrl + "/" + id,
+			type: 'get',
+			contentType: "application/json; charset=utf-8",
+			success: function (device: ServerDevice) {
+				var found = false;
+				for (var i = 0; i < me.Devices().length; i++) {
+					var d = me.Devices()[i];
+					if (d.Id() == device.Id) {
+						me.Devices.replace(d, new Device(device));
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					me.Devices.push(new Device(device));
+				}
+			}
+		});
+	}
+
+	public AddNewDevice() {
+		this.CurrentDevice(new Device());
+	}
+
+	public SaveNewDevice() {
+		var cd = this.CurrentDevice();
+		var errorMessage: string = "";
+		if (cd.Name().length <= 0) {
+			errorMessage = "Name is required.\n";
+		}
+		if (cd.Description().length <= 0) {
+			errorMessage += "Description is required.\n";
+		}
+
+		if (errorMessage.length > 0) {
+			alert(errorMessage);
+		}
+		else {
+			var me = this;
+			$.ajax({
+				url: me._apiUrl,
+				type: 'post',
+				contentType: "application/json; charset=utf-8",
+				data: JSON.stringify(cd.AsServerDevice()),
+				dataType: "json",
+				success: function (id) {
+					me.loadDevice(id);
+				}
+			});
+		}
+	}
 }
