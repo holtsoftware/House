5bb860057383ee3df8331e4243713dc48aabce4f
Adam Holt <holtsoftware@outlook.com>
2016-08-09 08:58:26 -0600

    Added ILoginViewModel

---
+++ Sannel.House.Client/Sannel.House.Client.Base/Interfaces/ILoginViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sannel.House.Client.Interfaces
+{
+	/// <summary>
+	/// What the login form needs
+	/// </summary>
+	public interface ILoginViewModel
+	{
+		/// <summary>
+		/// Gets or sets the username.
+		/// </summary>
+		/// <value>
+		/// The username.
+		/// </value>
+		String Username
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the password.
+		/// </summary>
+		/// <value>
+		/// The password.
+		/// </value>
+		String Password
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether to [stay logged in].
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if [stay logged in]; otherwise, <c>false</c>.
+		/// </value>
+		bool StayLoggedIn
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets the login command that preforms the login
+		/// </summary>
+		/// <value>
+		/// The login.
+		/// </value>
+		ICommand Login
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets the error keys for the current errors.
+		/// </summary>
+		/// <value>
+		/// The error keys.
+		/// </value>
+		ObservableCollection<String> ErrorKeys
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this instance has errors.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if this instance has errors; otherwise, <c>false</c>.
+		/// </value>
+		bool HasErrors
+		{
+			get;
+		}
+	}
+}
