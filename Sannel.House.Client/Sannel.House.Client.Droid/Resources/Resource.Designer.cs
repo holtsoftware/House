7e939c96e0d649fb66f9486bbd312c9e25f3d865
Adam Holt <holtsoftware@outlook.com>
2016-08-25 17:17:24 -0600

    Updates

--- Sannel.House.Client/Sannel.House.Client.Droid/Resources/Resource.Designer.cs
+++ Sannel.House.Client/Sannel.House.Client.Droid/Resources/Resource.Designer.cs
@@ -31,46 +31,12 @@ namespace Sannel.House.Client.Droid
 		public partial class Attribute
 		{
 			
-			// aapt resource value: 0x7f010000
-			public const int layoutManager = 2130771968;
-			
-			// aapt resource value: 0x7f010002
-			public const int reverseLayout = 2130771970;
-			
-			// aapt resource value: 0x7f010001
-			public const int spanCount = 2130771969;
-			
-			// aapt resource value: 0x7f010003
-			public const int stackFromEnd = 2130771971;
-			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Attribute()
-			{
-			}
-		}
-		
-		public partial class Dimension
-		{
-			
-			// aapt resource value: 0x7f040000
-			public const int item_touch_helper_max_drag_scroll_per_frame = 2130968576;
-			
-			// aapt resource value: 0x7f040001
-			public const int item_touch_helper_swipe_escape_max_velocity = 2130968577;
-			
-			// aapt resource value: 0x7f040002
-			public const int item_touch_helper_swipe_escape_velocity = 2130968578;
-			
-			static Dimension()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Dimension()
 			{
 			}
 		}
@@ -100,47 +66,44 @@ namespace Sannel.House.Client.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int LoginAction = 2131034116;
+			// aapt resource value: 0x7f060003
+			public const int LoginAction = 2131099651;
 			
-			// aapt resource value: 0x7f05000d
-			public const int action_websearch = 2131034125;
+			// aapt resource value: 0x7f06000c
+			public const int action_websearch = 2131099660;
 			
-			// aapt resource value: 0x7f050007
-			public const int busyOverlay = 2131034119;
+			// aapt resource value: 0x7f060006
+			public const int busyOverlay = 2131099654;
 			
-			// aapt resource value: 0x7f050006
-			public const int content_frame = 2131034118;
+			// aapt resource value: 0x7f060005
+			public const int content_frame = 2131099653;
 			
-			// aapt resource value: 0x7f05000c
-			public const int continueAction = 2131034124;
+			// aapt resource value: 0x7f06000b
+			public const int continueAction = 2131099659;
 			
-			// aapt resource value: 0x7f050005
-			public const int drawer_layout = 2131034117;
+			// aapt resource value: 0x7f060004
+			public const int drawer_layout = 2131099652;
 			
-			// aapt resource value: 0x7f050001
-			public const int errorView = 2131034113;
+			// aapt resource value: 0x7f060000
+			public const int errorView = 2131099648;
 			
-			// aapt resource value: 0x7f050000
-			public const int item_touch_helper_previous_elevation = 2131034112;
+			// aapt resource value: 0x7f060007
+			public const int left_drawer = 2131099655;
 			
-			// aapt resource value: 0x7f050008
-			public const int left_drawer = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int menuLayout = 2131099656;
 			
-			// aapt resource value: 0x7f050009
-			public const int menuLayout = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int menuText = 2131099657;
 			
-			// aapt resource value: 0x7f05000a
-			public const int menuText = 2131034122;
+			// aapt resource value: 0x7f060002
+			public const int password = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int password = 2131034115;
+			// aapt resource value: 0x7f06000a
+			public const int serverUrl = 2131099658;
 			
-			// aapt resource value: 0x7f05000b
-			public const int serverUrl = 2131034123;
-			
-			// aapt resource value: 0x7f050002
-			public const int username = 2131034114;
+			// aapt resource value: 0x7f060001
+			public const int username = 2131099649;
 			
 			static Id()
 			{
@@ -186,8 +149,8 @@ namespace Sannel.House.Client.Droid
 		public partial class Menu
 		{
 			
-			// aapt resource value: 0x7f070000
-			public const int navigation_drawer = 2131165184;
+			// aapt resource value: 0x7f050000
+			public const int navigation_drawer = 2131034112;
 			
 			static Menu()
 			{
@@ -202,38 +165,38 @@ namespace Sannel.House.Client.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int ApplicationName = 2131099649;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f06000a
-			public const int EndTimeIsRequired = 2131099658;
+			// aapt resource value: 0x7f04000a
+			public const int EndTimeIsRequired = 2130968586;
 			
-			// aapt resource value: 0x7f060005
-			public const int ErrorConnectingToTheServer = 2131099653;
+			// aapt resource value: 0x7f040005
+			public const int ErrorConnectingToTheServer = 2130968581;
 			
-			// aapt resource value: 0x7f060000
-			public const int Hello = 2131099648;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
-			// aapt resource value: 0x7f060006
-			public const int InvalidUsernameOrPassword = 2131099654;
+			// aapt resource value: 0x7f040006
+			public const int InvalidUsernameOrPassword = 2130968582;
 			
-			// aapt resource value: 0x7f060004
-			public const int PasswordIsRequired = 2131099652;
+			// aapt resource value: 0x7f040004
+			public const int PasswordIsRequired = 2130968580;
 			
-			// aapt resource value: 0x7f060002
-			public const int ServerUrlError = 2131099650;
+			// aapt resource value: 0x7f040002
+			public const int ServerUrlError = 2130968578;
 			
-			// aapt resource value: 0x7f060009
-			public const int StartTimeIsRequired = 2131099657;
+			// aapt resource value: 0x7f040009
+			public const int StartTimeIsRequired = 2130968585;
 			
-			// aapt resource value: 0x7f060003
-			public const int UsernameMustBeEmail = 2131099651;
+			// aapt resource value: 0x7f040003
+			public const int UsernameMustBeEmail = 2130968579;
 			
-			// aapt resource value: 0x7f060008
-			public const int drawer_close = 2131099656;
+			// aapt resource value: 0x7f040008
+			public const int drawer_close = 2130968584;
 			
-			// aapt resource value: 0x7f060007
-			public const int drawer_open = 2131099655;
+			// aapt resource value: 0x7f040007
+			public const int drawer_open = 2130968583;
 			
 			static String()
 			{
@@ -241,41 +204,6 @@ namespace Sannel.House.Client.Droid
 			}
 			
 			private String()
-			{
-			}
-		}
-		
-		public partial class Styleable
-		{
-			
-			public static int[] RecyclerView = new int[] {
-					16842948,
-					2130771968,
-					2130771969,
-					2130771970,
-					2130771971};
-			
-			// aapt resource value: 0
-			public const int RecyclerView_android_orientation = 0;
-			
-			// aapt resource value: 1
-			public const int RecyclerView_layoutManager = 1;
-			
-			// aapt resource value: 3
-			public const int RecyclerView_reverseLayout = 3;
-			
-			// aapt resource value: 2
-			public const int RecyclerView_spanCount = 2;
-			
-			// aapt resource value: 4
-			public const int RecyclerView_stackFromEnd = 4;
-			
-			static Styleable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Styleable()
 			{
 			}
 		}
