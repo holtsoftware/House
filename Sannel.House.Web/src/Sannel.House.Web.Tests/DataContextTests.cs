6e3f921c8b866b0fd30411db2bb02f18ab87546b
Adam Holt <holtsoftware@outlook.com>
2016-11-24 14:53:43 -0700

    Upgraded to .net core 1.1 and visual studio 2017 RC

---
+++ Sannel.House.Web/src/Sannel.House.Web.Tests/DataContextTests.cs
@@ -0,0 +1,39 @@
+﻿using Sannel.House.Web.Base.Interfaces;
+using Sannel.House.Web.Base.Models;
+using Sannel.House.Web.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sannel.House.Web.Tests
+{
+	public class DataContextTests : IClassFixture<TestDbContext>
+	{
+
+		private TestDbContext testContext;
+
+		public DataContextTests(TestDbContext context)
+		{
+			this.testContext = context;
+		}
+
+		/// <summary>
+		/// Used to verify our mock database is setup correctly for testing.
+		/// </summary>
+		[Fact]
+		public void CountTest()
+		{
+			var context = testContext.DataContext;
+			context.Devices.Add(new Device
+			{
+				Id = 1,
+				Name = "Test1",
+				Description = "Test 1 Description"
+			});
+			context.SaveChanges();
+			Assert.Equal(1, context.Devices.Count());
+		}
+	}
+}
---
+++ Sannel.House.Web/src/Sannel.House.Web.Tests/TestDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Sannel.House.Web.Base.Interfaces;
+using Sannel.House.Web.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Sannel.House.Web.Tests
+{
+	public class TestDbContext : IDisposable
+	{
+		public IDataContext DataContext
+		{
+			get;
+			private set;
+		}
+		public TestDbContext()
+		{
+			DataContext = new MockDataContext();
+		}
+		public void Dispose()
+		{
+			var type = DataContext.GetType();
+			foreach(var prop in type.GetRuntimeProperties())
+			{
+				if (prop.PropertyType.Name.StartsWith("DbSet"))
+				{
+					// Lets cheat and use dynamic to reference the RemoveRange method.
+					dynamic d = prop.GetValue(DataContext);
+					d.RemoveRange(d);	
+				}
+			}
+			DataContext.SaveChanges();
+			DataContext?.Dispose();
+		}
+	}
+}
