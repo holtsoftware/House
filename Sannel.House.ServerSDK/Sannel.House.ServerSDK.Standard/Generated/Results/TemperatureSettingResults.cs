37da226f17deb1400b0a52ed1ee5ec1b0ce04cb4
Adam Holt <holtsoftware@outlook.com>
2016-12-12 22:50:52 -0700

    Updates

---
+++ Sannel.House.ServerSDK/Sannel.House.ServerSDK.Standard/Generated/Results/TemperatureSettingResults.cs
@@ -0,0 +1,64 @@
+/* Copyright 2016 Sannel Software, L.L.C.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sannel.House.ServerSDK
+{
+	public sealed class TemperatureSettingResults
+	{
+		public TemperatureSettingResults(RequestStatus status, IList<ITemperatureSetting> data, Int64 key)
+		{
+			Status = status;
+			Data = data;
+			Key = key;
+		}
+
+		public TemperatureSettingResults(RequestStatus status, IList<ITemperatureSetting> data, Int64 key, Exception exception)
+		{
+			Status = status;
+			Data = data;
+			Key = key;
+			Exception = exception;
+		}
+
+		public RequestStatus Status
+		{
+			get;
+			set;
+		}
+
+		public IList<ITemperatureSetting> Data
+		{
+			get;
+			set;
+		}
+
+		public Int64 Key
+		{
+			get;
+			set;
+		}
+
+		public Exception Exception
+		{
+			get;
+			set;
+		}
+	}
+}
